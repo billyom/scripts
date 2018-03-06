c5d0756dbce66e4b0253f450ecf0deb3e1496af1
Billy O'Mahony <billy.o.mahony@intel.com>
2018-03-06 14:53:39 +0000

    flow_dist: working!
    Distribute a bunch of flows over several of streams

--- trex/flow_dist.py
+++ trex/flow_dist.py
@@ -1,48 +1,40 @@
+#!/usr/bin/env python
+# get TRex APIs
+from trex_stl_lib.api import *
+
+import time
+import cmd
+import re
+
 import itertools
 import random
 import numbers
 import pprint
 
-primes_and_one = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 
-31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 
-73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 
-127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 
-179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 
+def pro_rate (total, ratios):
+    """
+    Split a total in a ratio.
+
+    >>> pro_rate (100, [85, 11, 3, 1])
+    [85, 11, 3, 1]
+
+    >>> pro_rate (2, [50, 50])
+    [1, 1]
+
+    >>> pro_rate (106, [41, 31, 21, 11])
+    [41, 31, 21, 11]
+
+    """
+    sum_ratios = sum(ratios)
+    return [(total*ratio)/sum_ratios for ratio in ratios]
+
+primes_and_one = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+73, 79, 83, 89, 97, 101, 103, 107, 109, 113,
+127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
 233, 239, 241, 251]
 
-"""
-257, 263, 269, 271, 277, 281, 
-283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 
-353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 
-419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 
-467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 
-547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 
-607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 
-661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 
-739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 
-811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 
-877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 
-947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 
-1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 
-1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151, 
-1153, 1163, 1171, 1181, 1187, 1193, 1201, 1213, 1217, 1223, 
-1229, 1231, 1237, 1249, 1259, 1277, 1279, 1283, 1289, 1291, 
-1297, 1301, 1303, 1307, 1319, 1321, 1327, 1361, 1367, 1373, 
-1381, 1399, 1409, 1423, 1427, 1429, 1433, 1439, 1447, 1451, 
-1453, 1459, 1471, 1481, 1483, 1487, 1489, 1493, 1499, 1511, 
-1523, 1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583, 
-1597, 1601, 1607, 1609, 1613, 1619, 1621, 1627, 1637, 1657, 
-1663, 1667, 1669, 1693, 1697, 1699, 1709, 1721, 1723, 1733, 
-1741, 1747, 1753, 1759, 1777, 1783, 1787, 1789, 1801, 1811, 
-1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889, 
-1901, 1907, 1913, 1931, 1933, 1949, 1951, 1973, 1979, 1987, 
-1993, 1997, 1999, 2003, 2011, 2017, 2027, 2029, 2039, 2053, 
-2063, 2069, 2081, 2083, 2087, 2089, 2099, 2111, 2113, 2129, 
-2131, 2137, 2141, 2143, 2153, 2161, 2179, 2203, 2207, 2213, 
-2221, 2237, 2239, 2243, 2251, 2267, 2269, 2273, 2281, 2287, 
-2293, 2297, 2309, 2311, 2333, 2339, 2341, 2347, 2351, 2357, 
-2371, 2377, 2381, 2383, 2389, 2393, 2399, 2411, 2417, 2423]
-"""
 
 from operator import mul
 
@@ -66,26 +58,11 @@ def n_as_product_of_primes (n, num_primes):
 
     return list(best_combo), best_diff
 
-def pro_rate (total, ratios):
-    """
-    Split a total in a ratio.
-
-    >>> pro_rate (100, [85, 11, 3, 1])
-    [85, 11, 3, 1]
-
-    >>> pro_rate (2, [50, 50])
-    [1, 1]
-
-    >>> pro_rate (106, [41, 31, 21, 11])
-    [41, 31, 21, 11]
-
-    """
-    sum_ratios = sum(ratios)
-    return [(total*ratio)/sum_ratios for ratio in ratios]
-
 
 def column_sum (lofl):
     """
+    DEPRECATED - use [sum (x) for x in zip(list1, list2, ...)]
+
     For a list of lists of numbers return a list whose ith element are the sum
     of the ith elements of each of the lists.
 
@@ -132,7 +109,223 @@ def tuple_spans_to_ranges (tuple_spans, span_base):
 
     return l_of_ranges
 
+def ip_inc (ip_addr_tuple, inc):
+    """
+    >>> ip_inc ((0, 0, 0, 0), 255)
+    '0.0.0.255'
 
+    >>> ip_inc ((0, 0, 0, 0), 256)
+    '0.0.1.0'
+
+    >>> ip_inc ((0, 0, 255, 513), 256)
+    '0.1.2.1'
+    """
+
+    ip = list (ip_addr_tuple)
+    ip[3] += inc
+
+    for idx in range(len(ip)-1, -1, -1):
+        carry, normalized = ip[idx]/256, ip[idx]%256
+        ip[idx] = normalized
+        if carry:
+            ip[idx-1] += carry
+
+    return ".".join([str(x) for x in ip])
+
+
+
+class MyCmd(cmd.Cmd):
+    def __init__(self, client):
+        cmd.Cmd.__init__(self)
+        self.client = client
+        self.stats_time = time.time()
+
+
+    def create_streams(self, num_streams, total_kpps, total_num_flows):
+        """
+        Create several streams with to arrived at the kpps and num_flows.
+
+        Each stream will have equal kpps but different number of flows.
+        """
+
+        # each stream will have half the number of flows as the previous stream
+        dist = [2**(x-1) for x in range(num_streams, 0, -1)]
+        tuple_len = 4 # src ip, src port, dst ip, dst port
+
+        stream_to_num_flows = pro_rate(total_num_flows, dist)
+        print "flows per stream:", stream_to_num_flows, "total:", sum(stream_to_num_flows)
+
+        stream_to_tuple_span = []
+	random.seed((num_streams, total_kpps, total_num_flows)) # always mix up tuple ranges repeatably
+        for stream_no, total_num_flows in enumerate (stream_to_num_flows):
+            primes, diff = n_as_product_of_primes (total_num_flows, tuple_len)
+            random.shuffle(primes)
+            stream_to_tuple_span.append(primes)
+            print "stream#:", stream_no, "req. #flows:", total_num_flows, "tuple_spans:", primes, "diff: ", diff
+
+        span_base = [0, 0, 0, 0]
+        tuple_ranges = tuple_spans_to_ranges(stream_to_tuple_span, span_base)
+
+        #pp = pprint.PrettyPrinter(indent=4)
+        #pp.pprint (tuple_ranges)
+
+        # IP range
+        src_ip_start = (16, 0, 0, 0)
+        dst_ip_start = (48, 0, 0, 0)
+        SRC_IP = 0
+        SRC_PORT = 1
+        DST_IP = 2
+        DST_PORT = 3
+        RANGE_START = 0
+        RANGE_END = 1
+        streams = []
+        for stream_no, tuple_range in enumerate(tuple_ranges):
+            print "stream#:", stream_no, "src:",                           \
+                ip_inc(src_ip_start, tuple_range[SRC_IP][RANGE_START]),    \
+                "-",                                                       \
+                ip_inc(src_ip_start, tuple_range[SRC_IP][RANGE_END]),      \
+                ":", tuple_range[SRC_PORT][RANGE_START], "-",              \
+                tuple_range[SRC_PORT][RANGE_END],                          \
+                "dst",                                                     \
+                ip_inc(dst_ip_start, tuple_range[DST_IP][RANGE_START]),    \
+                "-",                                                       \
+                ip_inc(dst_ip_start, tuple_range[DST_IP][RANGE_END]),      \
+                ":", tuple_range[DST_PORT][RANGE_START], "-",              \
+                tuple_range[DST_PORT][RANGE_END]
+
+            # construct the variable part of the packet
+            vm = STLVM()
+
+            # define vars
+            vm.var(name="src",                                                       \
+                   min_value=ip_inc(src_ip_start, tuple_range[SRC_IP][RANGE_START]), \
+                   max_value=ip_inc(src_ip_start, tuple_range[SRC_IP][RANGE_END]),   \
+                   size=4,op="inc")
+            vm.var(name="dst",                                                       \
+                   min_value=ip_inc(dst_ip_start, tuple_range[DST_IP][RANGE_START]), \
+                   max_value=ip_inc(dst_ip_start, tuple_range[DST_IP][RANGE_END]),   \
+                   size=4,op="inc")
+            vm.var(name="srcport", min_value=tuple_range[SRC_PORT][RANGE_START],     \
+                    max_value=tuple_range[SRC_PORT][RANGE_END],                      \
+                    size=2,op="inc")
+            vm.var(name="dstport", min_value=tuple_range[DST_PORT][RANGE_START],     \
+                    max_value=tuple_range[DST_PORT][RANGE_END],                      \
+                    size=2,op="inc")
+
+
+            # write vars
+            vm.write(fv_name="src",pkt_offset= "IP.src")
+            vm.write(fv_name="dst",pkt_offset= "IP.dst")
+            vm.write(fv_name="srcport",pkt_offset= "UDP.sport")
+            vm.write(fv_name="dstport",pkt_offset= "UDP.dport")
+
+	    # ensure valid csum - voodoo from syn_attack_fix_cs_hw.py
+            vm.fix_chksum_hw(l3_offset="IP",
+			    l4_offset = "UDP",
+			    l4_type  = CTRexVmInsFixHwCs.L4_TYPE_UDP )
+
+            # construct the fixed part of the packet
+            size = 64
+            pps = total_kpps*1000/num_streams
+            isg = 0
+
+            base_pkt = Ether()/IP()/UDP()
+            pad = max(0, size - len(base_pkt)) * 'x'
+
+            pkt = STLPktBuilder(pkt = base_pkt/pad,
+                                vm = vm)
+
+            streams.append(STLStream(isg = isg,
+                            packet = pkt,
+                            mode = STLTXCont(pps = pps)))
+
+        return streams
+
+
+
+    def do_start(self, line):
+        """start kpps #flows
+        """
+        (argc, argv) = self._parse(line)
+        try:
+            total_num_flows = int(argv[1])
+            total_kpps = int(argv[0])
+        except:
+            self.do_help('start')
+            return 0
+
+        self._do_clear()
+
+        num_streams = 5
+        streams = self.create_streams(num_streams, total_kpps, total_num_flows)
+        stream_ids = self.client.add_streams(streams, ports=[0])
+        self.client.start(ports=[0], force=True)
+        self._print_warnings()
+
+
+    def do_stop(self, line):
+        """Stop all traffic"""
+        rc = self.client.stop(rx_delay_ms=100) #returns None
+
+
+    def do_clear(self, line):
+        """Clear stats"""
+        self._do_clear()
+
+
+    def _do_clear(self):
+        self.client.clear_stats() #returns None
+        self.stats_time = time.time()
+
+
+    def do_quit(self, line):
+        return 1
+
+
+    def emptyline(self):
+        return
+
+
+    def _parse(self, line):
+        argv = line.split()
+        argc = len(argv)
+        return (argc, argv)
+
+
+    def _print_warnings(self):
+        for w in self.client.get_warnings():
+            print w
+            print type (w)
+
+
+def main():
+    # connect to the server
+    c = STLClient(server = '127.0.0.1')
+
+    try:
+        # connect to the server
+        c.connect()
+
+        # fetch the TRex server version
+        ver = c.get_server_version()
+
+        print(ver)
+        res = c.reset()
+        res = c.set_port_attr(c.get_all_ports(), promiscuous=True)
+        MyCmd(c).cmdloop()
+
+    except STLError as e:
+        print(e)
+
+    finally:
+        c.disconnect()
+
+
+if __name__ == '__main__':
+    main()
+
+
+"""
 num_flows = 1000000
 dist=[1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1]
 num_buckets = len(dist)
@@ -153,4 +346,4 @@ tuple_ranges = tuple_spans_to_ranges (bucket_to_tuple_span, span_base)
 
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint (tuple_ranges)
-
+"""
